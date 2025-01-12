644dc4ae776cbade3d1751db13d502b270cf53d6
tang_jingfan <tangjingfan@gmail.com>
2025-01-13 00:56:10 +0800

    feat: update slam

--- cartographer_ros/configuration_files/revo_lds.lua
+++ cartographer_ros/configuration_files/revo_lds.lua
@@ -11,16 +11,16 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-
+ 
 include "map_builder.lua"
 include "trajectory_builder.lua"
-
+ 
 options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "horizontal_laser_link",
-  published_frame = "horizontal_laser_link",
+  tracking_frame = "laser",
+  published_frame = "laser",
   odom_frame = "odom",
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
@@ -42,9 +42,9 @@ options = {
   imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
 }
-
+ 
 MAP_BUILDER.use_trajectory_builder_2d = true
-
+ 
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
 TRAJECTORY_BUILDER_2D.min_range = 0.3
 TRAJECTORY_BUILDER_2D.max_range = 8.
@@ -54,9 +54,9 @@ TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
-
+ 
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 POSE_GRAPH.optimize_every_n_nodes = 35
 POSE_GRAPH.constraint_builder.min_score = 0.65
-
+ 
 return options
